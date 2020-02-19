b69f6ffccabb0975d49abe9333a285233c5d62e3
Lup Gabriel <gwolf2u.website@gmail.com>
2020-02-19 19:42:44 +0200

    guacamole: drop AOSP SurfaceFlinger and disable mask layer hint thanks @Hikari-no-Tenshi for research

--- vendor_props.mk
+++ vendor_props.mk
@@ -6,14 +6,5 @@
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=560
-
-# SurfaceFlinger
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=500 \
-    ro.surface_flinger.set_touch_timer_ms=750 \
-    ro.surface_flinger.set_display_power_timer_ms=1000 \
-    ro.surface_flinger.use_smart_90_for_video=true \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.sf.lcd_density=560 \
+    vendor.display.disable_mask_layer_hint=1
