8c27032239341551a16237886c8a393c40bafac7
Demon000 <demonsingur@gmail.com>
2020-04-19 13:09:43 +0200

    guacamole: Leave space for FOD while bouncer is requesting Pattern on PIN
    Change-Id: Iddfa076d92deaec982c5fe8711e155648da89965

--- device.mk
+++ device.mk
@@ -13,7 +13,8 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Audio
 PRODUCT_COPY_FILES += \
