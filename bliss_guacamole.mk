4cba7f5d367903d31d004839a216c1221ab0f2d0
Joey Huab <joey@evolution-x.org>
2020-08-29 23:17:23 -0500

    guacamole: Update to Coral's August fingerprint

--- bliss_guacamole.mk
+++ bliss_guacamole.mk
@@ -44,7 +44,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 TARGET_HAS_FOD := true
 
 # Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/2007240000:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7Pro \
     PRODUCT_NAME=OnePlus7Pro \
