d0770c313785e93d535a2e053742fd2dbeedd53f
srgrusso <srgrusso240@gmail.com>
2021-01-01 15:56:10 -0600

    Blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_guacamole.mk
+    $(LOCAL_DIR)/bliss_guacamole.mk
 
 COMMON_LUNCH_CHOICES := \
-	yaap_guacamole-user \
-	yaap_guacamole-userdebug \
-	yaap_guacamole-eng
+	bliss_guacamole-user \
+	bliss_guacamole-userdebug \
+	bliss_guacamole-eng
