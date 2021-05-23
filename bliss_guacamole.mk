ee7cf763455ce43b1f4a235112b0c98d167d7031
srgrusso <srgrusso240@gmail.com>
2021-05-22 19:56:00 -0500

    guacamole:Set flag for FOD animations

--- bliss_guacamole.mk
+++ bliss_guacamole.mk
@@ -17,6 +17,9 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1440
 
+# FOD animations
+TARGET_WANTS_FOD_ANIMATIONS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := bliss_guacamole
 PRODUCT_DEVICE := guacamole
