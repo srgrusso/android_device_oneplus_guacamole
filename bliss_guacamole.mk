693eadaf19b0696dd1fed45dd0cec4133a613381
srgrusso <srgrusso240@gmail.com>
2024-10-27 11:27:14 -0500

    Blissify

--- bliss_guacamole.mk
+++ bliss_guacamole.mk
@@ -11,11 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1440
+
+BLISS_BUILDTYPE := OFFICIAL
+
+# UDFPS animations
+EXTRA_UDFPS_ANIMATIONS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_guacamole
+PRODUCT_NAME := bliss_guacamole
 PRODUCT_DEVICE := guacamole
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := GM1911
