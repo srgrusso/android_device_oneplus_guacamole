4ff8cef192727f99be6c4b369634d17941ea3189
srgrusso <srgrusso240@gmail.com>
2025-04-19 06:20:53 -0500

    Blissify

--- lineage_guacamole.mk
+++ lineage_guacamole.mk
@@ -15,6 +15,14 @@ $(call inherit-product, device/oneplus/guacamole/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+BLISS_BUILDTYPE := OFFICIAL
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# UDFPS animations
+EXTRA_UDFPS_ANIMATIONS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_guacamole
 PRODUCT_DEVICE := guacamole
